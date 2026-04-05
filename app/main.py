f784e16a5879dee317b7f7136da932cd49c11bbd
mduardo1 <moyseseduardo@icloud.com>
2026-04-05 11:03:14 -0300

    feat: connect auth routes to main app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from app.routes.auth_routes import auth_bp
 
 
 def create_app():
     app = Flask(__name__)
+
+    # 🔥 REGISTRA AS ROTAS
+    app.register_blueprint(auth_bp)
 
     @app.route("/")
     def home():
